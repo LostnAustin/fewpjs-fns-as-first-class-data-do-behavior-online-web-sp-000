4cc8e832d37c38afe04f0dccf27cdbf52991328f
Chad Lengefeld <chadlengefeld@gmail.com>
2020-08-02 07:13:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ function handleClick(e) {
 /* Write your implementation of greet() */
 
 function greet(String) {
-  
+
   const time = parseInt(String);
   if (time < 12 && time > 0)
     return "Good Morning"
-  if time == 12 && time > 17)
+  if (time == 12 && time > 17)
     return "Good Afternoon"
   if (time > 17 && time < 25)
     return "Good Evening"
